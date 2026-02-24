caa33a9c78bc6042ccd1f603c868f2afc97dc610
Jonathan Cabera <jon.svj63@gmail.com>
2026-02-23 22:25:14 -0500

    finished registerDiagnosticListener to update errors everytime vs once on load

--- hello-world/src/backend/DiagnosticListener.ts
+++ hello-world/src/backend/DiagnosticListener.ts
@@ -1,3 +1,31 @@
+import * as path from "path"
 import * as vscode from "vscode";
 
-export function registerDiagnosticListener(context: vscode.ExtensionContext) {}
+export function registerDiagnosticListener(
+  context: vscode.ExtensionContext,
+  activeEditor: vscode.TextEditor,
+  onErrorChange: (
+    error: { fileName: string; lineNumber: number; message: string } | null,
+  ) => void,
+) {
+    const eventListener = vscode.languages.onDidChangeDiagnostics((e) => {
+        const activeEditorUri = activeEditor.document.uri;
+        if (e.uris.some((uri) => uri.toString() === activeEditorUri.toString())) {
+            const editorDiagnostics = vscode.languages.getDiagnostics(activeEditorUri);
+            const filteredDiagnostics = editorDiagnostics.filter(
+                (diagnostic) =>
+                    diagnostic.severity === vscode.DiagnosticSeverity.Error,
+            );
+            if (filteredDiagnostics.length === 0) {
+                onErrorChange(null);
+                return;
+            }
+            onErrorChange({
+                fileName: path.basename(activeEditor.document.fileName),
+                lineNumber: filteredDiagnostics[0].range.start.line + 1,
+                message: filteredDiagnostics[0].message,
+            });
+        }
+    });
+    context.subscriptions.push(eventListener);
+}
